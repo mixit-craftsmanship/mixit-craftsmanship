a78a10834f4f7026e5aa207c3217be28e85de46b
Clément Bouillier <clement.bouillier@devcrafting.com>
2014-04-19 19:42:03 +0200

    Refacto des tests unitaires pour plus de clarté

--- test/public/viewModels/homeTest.js
+++ test/public/viewModels/homeTest.js
@@ -1,10 +1,10 @@
 define(['viewModels/home', 'libs/api'], function(home, api) {
     describe('Home ViewModels', function () {
-        it('when starting, should be', function () {
+        it('when starting, should be waiting for data from server', function () {
             api.currentTasks = {
                 done: function() {
                 }
-            }
+            };
             home.create().waiting().should.equal(true);
         });
     });
