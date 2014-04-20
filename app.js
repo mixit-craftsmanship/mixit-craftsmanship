dc7b18278d5e110cd2f54895bd77fac1c5284c4d
Florent PELLET <florent.pellet@gmail.com>
2014-04-20 16:30:58 +0200

    Update express and remove crypto and path of package.json (core modules)

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var talks = require('./routes/talks');
 var http = require('http');
 var path = require('path');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var staticFavicon = require('static-favicon');
+var errorhandler = require('errorhandler');
+var morgan = require('morgan');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+app.use(staticFavicon());
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/javascripts", express.static(path.join(__dirname, 'bower_components')));
 app.use("/javascripts", express.static(path.join(__dirname, 'node_modules/knockout/build/output')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+  app.use(errorhandler());
 }
 
-app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/api/talks/current', talks.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
