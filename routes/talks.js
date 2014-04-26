f712d72dd8b0005b083d764152670cc45edc394a
Florent PELLET <florent.pellet@gmail.com>
2014-04-26 12:59:37 +0200

    disable cache on talks

--- routes/talks.js
+++ routes/talks.js
@@ -2,6 +2,10 @@ var talksRepository = require('../libs/talksRepository');
 
 exports.list = function (req, res) {
     talksRepository.currentTalks().then(function (talks) {
+        res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.header("Pragma", "no-cache");
+        res.header("Expires", 0);
+
         res.send(talks);
     }).catch(function(error){
         res.send(500, error);
