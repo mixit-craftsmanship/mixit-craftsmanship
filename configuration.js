a369f041975254841259b9741bfee926d326cc2e
Florent PELLET <florent.pellet@gmail.com>
2014-04-26 13:45:00 +0200

    use jquery bower package

--- configuration.js
+++ configuration.js
@@ -1,6 +1,6 @@
 exports.externalLibs = {
     knockout: 'node_modules/knockout/build/output/knockout-latest.debug.js',
-    jquery: 'node_modules/jquery/dist/jquery.js',
+    jquery: 'bower_components/jquery/dist/jquery.js',
     socketIO: 'node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js',
     require: 'node_modules/requirejs/require.js'
 };
