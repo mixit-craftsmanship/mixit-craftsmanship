6b4df3eec54bdb7a1fbb25de3b397805af2e26c5
Florent PELLET <florent.pellet@gmail.com>
2014-04-28 01:11:07 +0200

    refactor talkVote

--- public/javascripts/viewModels/talkVote.js
+++ public/javascripts/viewModels/talkVote.js
@@ -8,28 +8,28 @@ define(['knockout', 'libs/timer'], function (ko, timerFactory) {
 
         self.happyLevel = ko.observable(0);
 
-        var clickNb = 0;
+        var voteNb = 0;
 
         var updateHappyLevel = function(){
-            if(clickNb > 8) self.happyLevel(5);
-            else if(clickNb > 6) self.happyLevel(4);
-            else if(clickNb > 4) self.happyLevel(3);
-            else if(clickNb > 1) self.happyLevel(2);
-            else if(clickNb == 1) self.happyLevel(1);
+            if(voteNb > 8) self.happyLevel(5);
+            else if(voteNb > 6) self.happyLevel(4);
+            else if(voteNb > 4) self.happyLevel(3);
+            else if(voteNb > 1) self.happyLevel(2);
+            else if(voteNb == 1) self.happyLevel(1);
             else self.happyLevel(0);
 
-            clickNb = Math.floor(clickNb / 2);
+            voteNb = Math.floor(voteNb / 2);
         };
 
         var timer = timerFactory.create(500, updateHappyLevel);
 
         self.vote = function(){
-            if(clickNb <= 0) {
+            if(voteNb <= 0) {
                 timer.restart();
                 self.happyLevel(1);
             }
 
-            clickNb++;
+            voteNb++;
         };
     };
 
