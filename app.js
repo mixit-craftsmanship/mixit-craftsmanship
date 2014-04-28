d7c6b9dc159cc42a5c5ba52e57815d4819ef792d
Florent PELLET <florent.pellet@gmail.com>
2014-04-28 12:16:15 +0200

    move static files routing to route

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var http = require('http');
-var path = require('path');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var staticFavicon = require('static-favicon');
-var errorHandler = require('errorhandler');
 var morgan = require('morgan');
 var socketIO = require('socket.io');
 var compression = require('compression');
 
-var globalConfiguration = require('./libs/globalConfiguration');
+var staticFilesRoute = require('./routes/staticFiles');
 var talksRoute = require('./routes/talks');
 var talkVotesRoute = require('./routes/talkVotes');
 
@@ -24,37 +22,10 @@ app.use(morgan('dev'));
 app.use(bodyParser());
 app.use(methodOverride());
 
-var staticFile = function(path){
-    return function(req, res, next) {
-        if ('GET' != req.method && 'HEAD' != req.method) {
-            return next();
-        }
-
-        res.sendfile(path);
-    };
-};
+var env = process.env.NODE_ENV || 'development';
 
 talksRoute.register(app);
-
-var env = process.env.NODE_ENV || 'development';
-if ('development' == env) {
-    app.use(errorHandler());
-
-    app.use(express.static(path.join(__dirname, 'public')));
-
-    var staticItems = globalConfiguration.client.getProxyUrlsInDevelopment();
-    for(var key in staticItems){
-        var item = staticItems[key];
-        if(item.file !== undefined){
-            app.use(item.url, staticFile(item.file));
-        } else {
-            app.use(item.url, express.static(item.directory));
-        }
-    }
-} else {
-    app.use(express.static(path.join(__dirname, 'publicBuild')));
-}
-
+staticFilesRoute.register(app, 'development' == env);
 
 var server = http.createServer(app);
 socketIO = socketIO.listen(server);
---
+++ routes/staticFiles.js
@@ -0,0 +1,38 @@
+var globalConfiguration = require('./../libs/globalConfiguration');
+var errorHandler = require('errorhandler');
+var express = require('express');
+
+var staticFile = function(path){
+    return function(req, res, next) {
+        if ('GET' != req.method && 'HEAD' != req.method) {
+            return next();
+        }
+
+        res.sendfile(path);
+    };
+};
+
+var configDev = function(app){
+    app.use(errorHandler());
+
+    app.use(express.static('public'));
+
+    var staticItems = globalConfiguration.client.getProxyUrlsInDevelopment();
+    for(var key in staticItems){
+        var item = staticItems[key];
+        if(item.file !== undefined){
+            app.use(item.url, staticFile(item.file));
+        } else {
+            app.use(item.url, express.static(item.directory));
+        }
+    }
+};
+
+var configProd = function(app){
+    app.use(express.static('publicBuild'));
+};
+
+exports.register = function(app, isDevelopment){
+    if(isDevelopment) configDev(app);
+    else configProd(app);
+};
