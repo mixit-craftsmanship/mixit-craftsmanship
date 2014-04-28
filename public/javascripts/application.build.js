6c17cb7c262d69563fc8530a04f33ba768e410a4
Florent PELLET <florent.pellet@gmail.com>
2014-04-28 10:41:57 +0200

    fix sammy link for requirejs

--- public/javascripts/application.build.js
+++ public/javascripts/application.build.js
@@ -4,7 +4,7 @@ require.config({
         knockout: '//cdnjs.cloudflare.com/ajax/libs/knockout/3.1.0/knockout-min',
         jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min',
         socketIO: '//cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min',
-        sammy: '//cdnjs.cloudflare.com/ajax/libs/sammy.js/0.7.4/sammy.min.js'
+        sammy: '//cdnjs.cloudflare.com/ajax/libs/sammy.js/0.7.4/sammy.min'
     }
 });
 
