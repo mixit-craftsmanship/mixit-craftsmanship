9bd0c2db293a22cbc5a8b80b554d748a2f733509
Florent PELLET <florent.pellet@gmail.com>
2014-04-29 17:38:23 +0200

    Create statistique repository

--- libs/statistiqueRepository.js
+++ libs/statistiqueRepository.js
@@ -1,3 +1,31 @@
-exports.getStatistiqueByTalksAndMinutes = function(){
+var mongoWrapper = require('./mongoWrapper');
+var mongoConfiguration = require('./globalConfiguration').mongo;
+var _ = require('underscore');
 
+var cloneAndReformatPeriod = function(item){
+    item = _.clone(item);
+    delete item.second;
+
+    item.day = item.day === 2 ? 29 : 30;
+
+    return item;
+};
+
+exports.getStatistiqueByTalksAndMinutes = function(){
+    return mongoWrapper.getAllItemsOfCollection(mongoConfiguration.getTalkVotesCollectionName()).then(function(result){
+        var itemsWithoutSeconds = _.map(result, cloneAndReformatPeriod);
+        var itemsGroupByTalkIdAndDate = _.groupBy(itemsWithoutSeconds, function(item){
+            return item.talkId + '-' + item.day + '-' + item.hour + '-' + item.minute;
+        });
+        return _.map(itemsGroupByTalkIdAndDate, function(items){
+            return _.reduce(items, function(acc, item){
+                if(acc === undefined) {
+                    return item;
+                }
+
+                acc.nb += item.nb;
+                return acc;
+            });
+        });
+    });
 };
--- test/server/libs/statistiqueRepositoryTest.js
+++ test/server/libs/statistiqueRepositoryTest.js
@@ -1,3 +1,99 @@
-/**
- * Created by Florent on 29/04/2014.
- */
+var statistiqueRepository = require('../../../libs/statistiqueRepository');
+var mongoWrapper = require('../../../libs/mongoWrapper');
+var mongoConfiguration = require('../../../libs/globalConfiguration').mongo;
+var promise = require('promise');
+
+describe('Statistique repository', function() {
+    var oldGetAllItemsOfCollection = mongoWrapper.getAllItemsOfCollection;
+    var oldConfigurationGetCollectionName = mongoConfiguration.getTalkVotesCollectionName;
+    after(function () {
+        mongoWrapper.getAllItemsOfCollection = oldGetAllItemsOfCollection;
+        mongoConfiguration.getTalkVotesCollectionName = oldConfigurationGetCollectionName;
+    });
+
+    var mongoResults = [];
+    beforeEach(function(){
+        mongoResults = [];
+        mongoWrapper.getAllItemsOfCollection = function(collectionName){
+            return promise.resolve(mongoResults);
+        };
+    });
+
+    it('When getStatistiqueByTalksAndMinutes Then call getAllItemsOfCollection of mongoWrapper with good collection', function () {
+        mongoConfiguration.getTalkVotesCollectionName = function(){
+            return 'talkVote';
+        };
+        var collectionNameUsed;
+        mongoWrapper.getAllItemsOfCollection = function(collectionName){
+            collectionNameUsed = collectionName;
+            return promise.resolve([]);
+        };
+
+        statistiqueRepository.getStatistiqueByTalksAndMinutes();
+
+        collectionNameUsed.should.equal('talkVote');
+    });
+
+    it('Sum votes by minute', function(done){
+         mongoResults.push({ talkId: 428, minute: 25, hour: 12, second: 20, day: 2, nb: 1 });
+         mongoResults.push({ talkId: 428, minute: 25, hour: 12, second: 25, day: 2, nb: 2 });
+
+         statistiqueRepository.getStatistiqueByTalksAndMinutes().then(function(result){
+             result.should.have.lengthOf(1);
+             var firstValue = result[0];
+             firstValue.should.eql({
+                 talkId: 428,
+                 minute: 25,
+                 hour: 12,
+                 day: 29,
+                 nb: 3
+             });
+
+             done();
+         }).catch(done);
+    });
+
+    it('Given 2 mongo items with not same talk Then return 2 items', function(done){
+        mongoResults.push({ talkId: 428, minute: 25, hour: 12, second: 20, day: 2, nb: 1 });
+        mongoResults.push({ talkId: 429, minute: 25, hour: 12, second: 25, day: 2, nb: 2 });
+
+        statistiqueRepository.getStatistiqueByTalksAndMinutes().then(function(result){
+            result.should.have.lengthOf(2);
+
+            done();
+        }).catch(done);
+    });
+
+    it('Given 2 mongo items with not same minute Then return 2 items', function(done){
+        mongoResults.push({ talkId: 428, minute: 25, hour: 12, second: 20, day: 2, nb: 1 });
+        mongoResults.push({ talkId: 428, minute: 26, hour: 12, second: 25, day: 2, nb: 2 });
+
+        statistiqueRepository.getStatistiqueByTalksAndMinutes().then(function(result){
+            result.should.have.lengthOf(2);
+
+            done();
+        }).catch(done);
+    });
+
+    it('Given item with day 2 Then return 29', function(done){
+        mongoResults.push({ talkId: 428, minute: 25, hour: 12, second: 20, day: 2, nb: 1 });
+
+        statistiqueRepository.getStatistiqueByTalksAndMinutes().then(function(result){
+            result.should.have.lengthOf(1);
+            result[0].day.should.equal(29);
+
+            done();
+        }).catch(done);
+    });
+
+    it('Given item with day 3 Then return 30', function(done){
+        mongoResults.push({ talkId: 428, minute: 25, hour: 12, second: 20, day: 3, nb: 1 });
+
+        statistiqueRepository.getStatistiqueByTalksAndMinutes().then(function(result){
+            result.should.have.lengthOf(1);
+            result[0].day.should.equal(30);
+
+            done();
+        }).catch(done);
+    });
+});
