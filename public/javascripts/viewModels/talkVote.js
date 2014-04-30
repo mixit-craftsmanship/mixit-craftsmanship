9ac31aa0edcf78a4b64488a6ce7d612abe66cad4
Damien MORELLET <morellet.d@sfeir.com>
2014-04-30 15:55:47 +0200

    Voir la description des sessions en cours

--- public/javascripts/viewModels/talkVote.js
+++ public/javascripts/viewModels/talkVote.js
@@ -1,4 +1,4 @@
-define(['knockout', 'libs/timer', 'libs/voteSender'], function (ko, timerFactory, voteSender) {
+define(['knockout', 'libs/timer', 'libs/voteSender', 'libs/api'], function (ko, timerFactory, voteSender, api) {
     var viewmodel = function(talkId, talkTitle){
         var self = this;
 
@@ -10,6 +10,15 @@ define(['knockout', 'libs/timer', 'libs/voteSender'], function (ko, timerFactory
         self.connected = ko.observable(false);
         self.talkEnded = ko.observable(false);
         self.happyLevel = ko.observable(0);
+
+	    self.loading = ko.observable(true);
+	    self.talkSummary = ko.observable();
+	    self.talkDay = ko.observable();
+	    self.talkStart = ko.observable();
+	    self.talkEnd = ko.observable();
+	    self.talkRoom = ko.observable();
+	    self.talkSpeakers = ko.observableArray();
+	    self.talkHasStartAndEnd = ko.observable(true);
 
         var voteNb = 0;
 
@@ -25,6 +34,22 @@ define(['knockout', 'libs/timer', 'libs/voteSender'], function (ko, timerFactory
         };
 
         var timer = timerFactory.create(500, updateHappyLevel);
+
+	    api.getTalk(talkId).done(function(talk){
+		    self.loading(false);
+		    self.talkSummary(talk.summary);
+		    if(talk.start === undefined || talk.end === undefined) {
+			    self.talkHasStartAndEnd(false);
+		    } else {
+			    var start = new Date(talk.start);
+			    self.talkDay(start.getDate() + "/" + (start.getMonth() + 1));
+			    self.talkStart(start.getHours() + ":" + zeroFill(start.getMinutes(), 2));
+			    var end = new Date(talk.end);
+			    self.talkEnd(end.getHours() + ":" + end.getMinutes());
+		    }
+		    self.talkRoom(talk.room);
+		    self.talkSpeakers(talk.speakers);
+	    });
 
         self.vote = function(){
             if(!voteSender.isEnabled()){
@@ -56,6 +81,16 @@ define(['knockout', 'libs/timer', 'libs/voteSender'], function (ko, timerFactory
             voteSender.disable();
         };
     };
+
+	function zeroFill(number, width)
+	{
+		width -= number.toString().length;
+		if ( width > 0 )
+		{
+			return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+		}
+		return number + ""; // always return a string
+	}
 
     return {
         create: function(talkId, talkTitle){
