51f314ce9b9c07b2688a6eec12e4d41a60dc5530
Florent PELLET <florent.pellet@gmail.com>
2014-05-08 20:55:48 +0200

    replace dx.chartjs by highcharts

--- public/javascripts/application.build.js
+++ public/javascripts/application.build.js
@@ -6,9 +6,8 @@ require.config({
         socketIO: '//cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min',
         sammy: '//cdnjs.cloudflare.com/ajax/libs/sammy.js/0.7.4/sammy.min',
         bootstrap: 'http://ajax.aspnetcdn.com/ajax/bootstrap/3.1.1/bootstrap.min',
-        chart: 'http://cdn3.devexpress.com/jslib/13.2.9/js/dx.chartjs',
-        globalize: 'http://ajax.aspnetcdn.com/ajax/globalize/0.1.1/globalize.min',
-        twitter: '//platform.twitter.com/widgets'
+        twitter: '//platform.twitter.com/widgets',
+        highcharts: '//cdnjs.cloudflare.com/ajax/libs/highcharts/4.0.1/highcharts'
     },
     shim: {
         sammy: {
