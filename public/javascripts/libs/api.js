ce93e3e0f23619f0dc6490bbeee03c32dd106af8
fpellet <florent.pellet@gmail.com>
2015-04-13 11:01:27 +0200

    Remove jquery of client api

--- public/javascripts/libs/api.js
+++ public/javascripts/libs/api.js
@@ -1,16 +1,35 @@
-define(['jquery'], function ($) {
+define(function () {
+    var ajaxGet = function(path, success) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                success(JSON.parse(xhr.responseText));
+            }
+        };
+        xhr.open('GET', path, true);
+        xhr.send();
+    };
+
+    var get = function(path){
+        return {
+            done: function(success) {
+                ajaxGet(path, success);
+            }
+        };
+    };
+
     return {
         currentTalks: function(){
-            return $.get("/api/talks/current");
+            return get("/api/talks/current");
         },
         nextTalks: function(){
-            return $.get("/api/talks/next");
+            return get("/api/talks/next");
         },
         getTalk: function(id){
-            return $.get("/api/talks/" + id);
+            return get("/api/talks/" + id);
         },
         applicationVersion: function(){
-            return $.get("/api/applicationVersion");
+            return get("/api/applicationVersion");
         }
     };
 });
